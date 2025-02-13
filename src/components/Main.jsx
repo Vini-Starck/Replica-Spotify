69e91d29c45b5e4ce40cadc79c23250f8e267e79
starck <starck.vinicius@gmail.com>
2025-02-13 05:27:04 -0300

    Mudanças na Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,30 +1,38 @@
-import React from 'react'
-import ItemList from './ItemList'
-import { artistArray } from '../assets/database/artists'
-import { songsArray } from '../assets/database/songs'
+import React from "react";
+import ItemList from "./ItemList";
+import { artistArray } from "../assets/database/artists";
+import { songsArray } from "../assets/database/songs";
 
-const Main = () => {
+const Main = ({ type }) => {
   return (
-    <div className='main'>
-
-        {/* ItemList de Artista */}
+    <div className="main">
+      {/* Item List de Artistas */}
+      {type === "artists" || type === undefined ? (
         <ItemList
-        title='Artistas'
-        items={5}
-        itemsArray={artistArray}
-        path="/artists"
-        idPath="/artist"/>
+          title="Artistas"
+          items={10}
+          itemsArray={artistArray}
+          path="/artists"
+          idPath="/artist"
+        />
+      ) : (
+        <></>
+      )}
 
-        {/* ItemList de Música */}
+      {/* Item List de Músicas */}
+      {type === "songs" || type === undefined ? (
         <ItemList
-        title='Músicas'
-        items={10}
-        itemsArray={songsArray}
-        path="/songs"
-        idPath="/song"/>
-
+          title="Músicas"
+          items={20}
+          itemsArray={songsArray}
+          path="/songs"
+          idPath="/song"
+        />
+      ) : (
+        <></>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
